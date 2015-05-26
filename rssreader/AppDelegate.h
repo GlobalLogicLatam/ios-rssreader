9ff0dc49c32e890d5baa933142b95d8e7dda949d
Gerson Villanueva <gerson.villanueva@globallogic.com>
2015-05-26 14:22:04 -0300

    Initial Commit

---
+++ rssreader/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  rssreader
+//
+//  Created by Gerson Villanueva on 5/26/15.
+//  Copyright (c) 2015 Gerson Villanueva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ rssreader/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  rssreader
+//
+//  Created by Gerson Villanueva on 5/26/15.
+//  Copyright (c) 2015 Gerson Villanueva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
